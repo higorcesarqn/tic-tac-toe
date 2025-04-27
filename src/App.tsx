3d0f8df2a3491c74c9af754c648ef82c9909601d
Higor César <higorcesarqn@gmail.com>
2025-04-27 20:11:35 -0300

    Add Prettier and Tailwind CSS plugin; update App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import "./index.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-900">Hello world!</h1>
+    <h1 className="mt-2 text-3xl font-bold text-red-900 underline">
+      Hello world!
+    </h1>
   );
 }
 
